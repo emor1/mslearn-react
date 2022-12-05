feff62dc45467bbccb826eac57885c7c5124ca01
emor1 <57478311+emor1@users.noreply.github.com>
2022-12-05 16:48:59 +0900

    mslearn 1

---
+++ code/0-starter/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import RecipeTitle from './RecipeTitle';
+import './index.css'
+
+function App() {
+    return (
+    <article>
+        <h1>Recipe Manager</h1>
+        <RecipeTitle/>
+    </article>
+    )
+}
+
+export default App;
