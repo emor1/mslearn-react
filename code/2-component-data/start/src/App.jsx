a981485f7708256a5b8e78fca415717eac878133
emor1 <57478311+emor1@users.noreply.github.com>
2022-12-08 19:38:48 +0900

    propsの基礎

--- code/2-component-data/start/src/App.jsx
+++ code/2-component-data/start/src/App.jsx
@@ -1,9 +1,34 @@
 import './index.css'
 import React from 'react';
 // TODO: Import RecipeTitle
+import RecipeTitle from './RecipeTitle';
+import IngredientList from './IngredientList';
+import RecipeStep from './RecipeStep';
 
 // TODO: Import IngredientList
-
+const recipe = {
+    title: 'Mashed potatoes',
+    feedback:{
+        rating: 4.8,
+        reviews: 20
+    },
+    ingredients:[
+        {name:'3 potatoes, cut into 1/2 pieces', prepared:false},
+        {name: '4 Tbsp butter', prepared:false},
+        {name: 'Salt', prepared:true},
+        {name: 'Pepper', prepared:true},
+    ],
+    steps:[
+        {step:"濃い塩水が入った鍋に、カットしたジャガイモを加えます。"},
+        {step:"鍋を沸騰させます。"},
+        {step:"フォークを刺せるくらいの柔らかさまでジャガイモを茹でます (約 15-20 分)。"},
+        {step:"ジャガイモをざるにあけます。"},
+        {step:"ジャガイモを鍋に戻します。"},
+        {step:"バター、クリーム、塩、こしょうを加えて好みの味にします。"},
+        {step:"ジャガイモをマッシュします。"},
+        {step:"必要に応じて味を調え、バターとクリームを追加します。"}
+    ]
+};
 
 function App() {
     // TODO: Add recipe object
@@ -12,9 +37,11 @@ function App() {
         <article>
             <h1>Recipe Manager</h1>
             {/* TODO: Add RecipeTitle component */}
-
+                <RecipeTitle title={recipe.title} feedback={recipe.feedback}/>
             {/* TODO: Add IngredientList component */}
+                <IngredientList ingredients={recipe.ingredients}/>
 
+                <RecipeStep steps={recipe.steps}/>
         </article>
     )
 }
